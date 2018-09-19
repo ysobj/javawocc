f8d336be3f1b3e66c83fca7c1218ea0b73cf87a7
ysobj <litailang+ysobj@gmail.com>
2018-09-19 12:27:57 +0900

    extract MethodInfo class and AttributeInfo class.

--- src/main/java/javawocc/Sample.java
+++ src/main/java/javawocc/Sample.java
@@ -2,6 +2,7 @@ package javawocc;
 
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.List;
 
 public class Sample {
 	public static void main(String[] args) throws Exception {
@@ -51,67 +52,122 @@ public class Sample {
 						+ "0006" // super class #6
 						+ "0000" // interface count = 0
 						+ "0000" // field count = 0
-						+ "0002" // method count = 2
-						// method[0]-->
-						+ "0001" // method[0] access_flag
-						+ "0007" // method[0] name_index
-						+ "0008" // method[0] descriptor_index
-						+ "0001" // method[0] attributes_count
-						// method[0].attribute[0]
-						// https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.7.3
-						+ "0009" // attribute_name_index #9 (Code)
-						+ "0000001d" // attribute_length
-						+ "0001" // max_stack
-						+ "0001" // max_locals
-						+ "00000005" // code_length
-						// code
-						+ "2a" // aload_0
-						+ "b7" + "0001" // invokespecial #1
-						+ "b1" // return
-						// code
-						+ "0000" // exception_table_length
-						+ "0001" // attribute_count
-						+ "000a" // attribute_name_index #10 (LineNumberTable)
-						+ "00000006" // attribute_length
-						+ "0001" // line_number_table_length
-						+ "0000" // start_pc[0]
-						+ "0002" // line_number[0]
-						// method[1]-->
-						+ "0009" // method[1] access_flag
-						+ "000b" // method[1] name_index
-						+ "000c" // method[1] descriptor_index
-						+ "0001" // method[1] attributes_count
-						// method[1].attribute[0]
-						+ "0009" // attribute_name_index #9 (Code)
-						+ "0000002a" // attribute_length
-						+ "0003" // max_stack
-						+ "0001" // max_locals
-						+ "0000000e" // *code_length
-						// code
-						+ "b2" + "0002" // getstatic #2
-						+ "11" + String.format("%04x", b) // sipush b
-						+ "11" + String.format("%04x", c) // sipush c
-						+ "60" // iadd
-						+ "b6" + "0004" // invokevirtual #4
-						+ "b1" // return
-						// code
-						+ "0000" // exception_table_length
-						+ "0001" // attribute_count
-						+ "000a" // attribute_name_index #10 (LineNumberTable)
-						+ "0000000a" // attribute_length
-						+ "0002" // line_number_table_length
-						+ "0000" // start_pc[0]
-						+ "0004" // line_number[0]
-						+ "0008" // start_pc[1]
-						+ "0005" // line_number[1]
-						//
-						+ "0001" // attribute_count
+						+ "0002"; // method count = 2
+
+		MethodInfo method0 = createMethod0();
+		bytes += method0.toString();
+		MethodInfo method1 = createMethod1(b, c);
+		bytes += method1.toString();
+		bytes +=
+
+				//
+				"0001" // attribute_count
 						+ "000d" // attribute_name_index
 						+ "00000002" // attribute_length
 						+ "000e";// info[2]
 		OutputStream os = new FileOutputStream(args[0] + "/javawocc/HelloWorld.class");
 		os.write(decode(bytes));
 		os.close();
+	}
+
+	private static MethodInfo createMethod0() {
+		// method[0]-->
+		String content = "0001" // method[0] access_flag
+				+ "0007" // method[0] name_index
+				+ "0008" // method[0] descriptor_index
+				+ "0001" // method[0] attributes_count
+				// method[0].attribute[0]
+				// https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.7.3
+				+ "0009" // attribute_name_index #9 (Code)
+				+ "0000001d" // attribute_length
+				+ "0001" // max_stack
+				+ "0001" // max_locals
+				+ "00000005" // code_length
+				// code
+				+ "2a" // aload_0
+				+ "b7" + "0001" // invokespecial #1
+				+ "b1" // return
+				// code
+				+ "0000" // exception_table_length
+				+ "0001" // attribute_count
+				+ "000a" // attribute_name_index #10 (LineNumberTable)
+				+ "00000006" // attribute_length
+				+ "0001" // line_number_table_length
+				+ "0000" // start_pc[0]
+				+ "0002"; // line_number[0]
+		MethodInfo method = new MethodInfo();
+		method.content = content;
+		return method;
+	}
+
+	private static MethodInfo createMethod1(int b, int c) {
+		String content = "0009" // method[1] access_flag
+				+ "000b" // method[1] name_index
+				+ "000c" // method[1] descriptor_index
+				+ "0001" // method[1] attributes_count
+				// method[1].attribute[0]
+				+ "0009" // attribute_name_index #9 (Code)
+				+ "0000002a" // attribute_length
+				+ "0003" // max_stack
+				+ "0001" // max_locals
+				+ "0000000e" // *code_length
+				// code
+				+ "b2" + "0002" // getstatic #2
+				+ "11" + String.format("%04x", b) // sipush b
+				+ "11" + String.format("%04x", c) // sipush c
+				+ "60" // iadd
+				+ "b6" + "0004" // invokevirtual #4
+				+ "b1" // return
+				// code
+				+ "0000" // exception_table_length
+				+ "0001" // attribute_count
+				+ "000a" // attribute_name_index #10 (LineNumberTable)
+				+ "0000000a" // attribute_length
+				+ "0002" // line_number_table_length
+				+ "0000" // start_pc[0]
+				+ "0004" // line_number[0]
+				+ "0008" // start_pc[1]
+				+ "0005"; // line_number[1]
+		MethodInfo method = new MethodInfo();
+		method.content = content;
+		return method;
+	}
+
+	private static class MethodInfo {
+		public String accessFlags;
+		public String nameIndex;
+		public String descriptorIndex;
+		public String attributesCount;
+		public List<AttributeInfo> attributes;
+		public String content;
+
+		@Override
+		public String toString() {
+			return content;
+		}
+	}
+
+	private static class AttributeInfo {
+		// u2 attribute_name_index;
+		// u4 attribute_length;
+		// u1 info[attribute_length];
+	}
+
+	private static class CodeAttributeInfo extends AttributeInfo {
+		// u2 attribute_name_index;
+		// u4 attribute_length;
+		// u2 max_stack;
+		// u2 max_locals;
+		// u4 code_length;
+		// u1 code[code_length];
+		// u2 exception_table_length;
+		// { u2 start_pc;
+		// u2 end_pc;
+		// u2 handler_pc;
+		// u2 catch_type;
+		// } exception_table[exception_table_length];
+		// u2 attributes_count;
+		// attribute_info attributes[attributes_count];
 	}
 
 	private static byte[] decode(String bytes) throws Exception {
