04489cfe68c2ce5108de41bd27d4131294a45b86
ysobj <litailang+ysobj@gmail.com>
2018-10-06 22:33:52 +0900

    introduce JavawoccParser.

---
+++ src/main/java/javawocc/parser/JavawoccParser.java
@@ -0,0 +1,35 @@
+package javawocc.parser;
+
+import java.util.List;
+
+import javawocc.ast.ASTNode;
+import javawocc.ast.ASTNodeList;
+import javawocc.ast.OperatorPrecedenceResolver;
+import javawocc.tokenizer.Tokenizer;
+
+public class JavawoccParser implements Parser {
+	private Parser parser;
+
+	public JavawoccParser() {
+		// factor = NUMBER | IDENTIFIER
+		// statement = factor (OPERATOR factor)
+
+		parser = new SequenceParser(new NumberParser(), new OneToManyParser(new OperatorParser(), new NumberParser())) {
+
+			@Override
+			protected ASTNode build(ASTNodeList node) {
+				List<ASTNode> list = node.getNodeList();
+				list.addAll(((ASTNodeList) list.remove(1)).getNodeList());
+				OperatorPrecedenceResolver resolver = new OperatorPrecedenceResolver();
+				return resolver.resolve(list);
+			}
+
+		};
+	}
+
+	@Override
+	public ASTNode parse(Tokenizer tokenizer) throws ParseException {
+		return parser.parse(tokenizer);
+	}
+
+}
