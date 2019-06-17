a977c5cd75174da0929146265776d62ef883d8c6
ysobj <litailang+ysobj@gmail.com>
2019-06-17 12:57:05 +0900

    just only add enum.

---
+++ src/main/java/javawocc/Instruction.java
@@ -0,0 +1,8 @@
+package javawocc;
+
+public enum Instruction {
+	iload, iload_0, iload_1, iload_2, iload_3, //
+	istore, istore_0, istore_1, istore_2, istore_3, //
+	ifeq, ifnull, iflt, ifle, ifne, ifnonnull, ifgt, ifge, //
+	ireturn
+}
